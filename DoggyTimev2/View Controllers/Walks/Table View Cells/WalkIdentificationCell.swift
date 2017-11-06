f872ddd07176bdb75494c96dccb901b7da245127
funkytomato <funkytomato@gmail.com>
2017-11-06 18:01:23 +0000

    Removed warning messages for printing the day of week

--- DoggyTimev2/View Controllers/Walks/Table View Cells/WalkIdentificationCell.swift
+++ DoggyTimev2/View Controllers/Walks/Table View Cells/WalkIdentificationCell.swift
@@ -27,30 +27,8 @@ class WalkIdentificationCell: UITableViewCell
             self.dayLbl.text = walk.dateofwalk?.dayOfWeek()
             self.locationLbl.text = walk.locationname
             
-            print(walk.dateofwalk?.dayOfWeek()!)
+            print(walk.dateofwalk?.dayOfWeek()! as Any)
             
          }
-    }
-}
-
-extension Date
-{
-    func dayOfWeek() -> String?
-    {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "EEEE"
-        return dateFormatter.string(from: self).capitalized
-        // or use capitalized(with: locale) if you want
-    }
-    
-    func hourOfDay() -> String?
-    {
-        //let hour = Calendar.current.component(.hour, from: self)
-        
-        let hourformatter = DateFormatter()
-        hourformatter.dateFormat = "h a"
-        hourformatter.amSymbol = "AM"
-        hourformatter.pmSymbol = "PM"
-        return hourformatter.string(from: self)
     }
 }
