c6d3190c5b51b85700976b2900c558b94409b6a6
Jason Fry <funkytomato@gmail.com>
2017-12-27 21:24:53 +0000

    Tracking routes implemented, untested

--- DoggyTimev2/Extensions/Maps/MapsViewController.swift
+++ DoggyTimev2/Extensions/Maps/MapsViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 
 class MapsViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate, UISearchBarDelegate
@@ -56,6 +57,8 @@ class MapsViewController: UIViewController, MKMapViewDelegate, CLLocationManager
     fileprivate var isCurrentLocation: Bool = false
     fileprivate var annotation: MKAnnotation!
 
+    //MARK:- Route variables
+    var previousLocation: CLLocation!
     
     //MARK:- Search variables
     
@@ -75,15 +78,18 @@ class MapsViewController: UIViewController, MKMapViewDelegate, CLLocationManager
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        
+
         let currentLocationButton = UIBarButtonItem(title: "Current Location", style: UIBarButtonItemStyle.plain, target: self, action: #selector(MapsViewController.currentLocationButtonAction(_:)))
         self.navigationItem.leftBarButtonItem = currentLocationButton
         
         let searchButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.search, target: self, action: #selector(MapsViewController.searchButtonAction(_:)))
         self.navigationItem.rightBarButtonItem = searchButton
         
+        //Config the mapview to show the user location
         mapView.delegate = self
         mapView.mapType = .hybrid
+        mapView.showsUserLocation = true
+        mapView.userTrackingMode = MKUserTrackingMode(rawValue: 2)!
         
         activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.gray)
         activityIndicator.hidesWhenStopped = true
@@ -109,11 +115,12 @@ class MapsViewController: UIViewController, MKMapViewDelegate, CLLocationManager
                 locationManager = CLLocationManager()
             }
             
-            locationManager?.requestWhenInUseAuthorization()
             locationManager.delegate = self
             locationManager.desiredAccuracy = kCLLocationAccuracyBest
             locationManager.requestAlwaysAuthorization()
+            locationManager?.requestWhenInUseAuthorization()
             locationManager.startUpdatingLocation()
+            locationManager.startUpdatingHeading()
             isCurrentLocation = true
         }
     }
@@ -167,11 +174,47 @@ class MapsViewController: UIViewController, MKMapViewDelegate, CLLocationManager
         }
     }
 
+    //Add annotation to map view
+    func addAnnotationsOnMap(locationToPoint: CLLocation)
+    {
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = locationToPoint.coordinate
+        let geoCoder = CLGeocoder()
+        geoCoder.reverseGeocodeLocation(locationToPoint, completionHandler: { (placemarks, error) -> Void in
+            if let placemarks = placemarks as? [CLPlacemark], placemarks.count > 0
+            {
+                let placemark = placemarks[0]
+                var addressDictionary = placemark.addressDictionary
+                annotation.title = addressDictionary!["Name"] as? String
+                self.mapView.addAnnotation(annotation)
+            }
+            
+        })
+    }
+    
+    
+    //MARK:- MKMapView delegate
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer
+    {
+        
+        if (overlay is MKPolyline)
+        {
+            let pr = MKPolylineRenderer(overlay: overlay)
+            pr.strokeColor = UIColor.red
+            pr.lineWidth = 5
+            return pr
+        }
+        
+        return MKOverlayRenderer()
+    }
+    
     
     
     // MARK: - CLLocationManagerDelegate
     
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation], newLocation: CLLocation!, fromLocation oldLocation: CLLocation!)
+    //func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
     {
         
         if !isCurrentLocation
@@ -200,5 +243,31 @@ class MapsViewController: UIViewController, MKMapViewDelegate, CLLocationManager
         pointAnnotation.title = ""
         mapView.addAnnotation(pointAnnotation)
         
+    
+        //Draw the route/path
+        if let oldLocationNew = oldLocation as CLLocation?
+        {
+            print("present location : \(newLocation.coordinate.latitude), \(newLocation.coordinate.longitude)")
+            
+            let oldCoordinates = oldLocationNew.coordinate
+            let newCoordinates = newLocation.coordinate
+            var area = [oldCoordinates, newCoordinates]
+            var polyline = MKPolyline(coordinates: &area, count: area.count)
+            mapView.add(polyline)
+        }
+        
+        //Calculation for location selection and pointing annotation
+        if let previousLocationNew = previousLocation as CLLocation?
+        {
+            addAnnotationsOnMap(locationToPoint: newLocation)
+            previousLocation = newLocation
+        }
+        else
+        {
+            //IN case previous location doesn't exist
+            addAnnotationsOnMap(locationToPoint: newLocation)
+            previousLocation = newLocation
+        }
+ 
     }
 }
